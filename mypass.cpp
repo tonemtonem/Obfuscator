5ba7e447e27f0f83199b62202d4e1da47b00d2e8
tonemtonem <kosstylev.ds@gmail.com>
2026-09-14 19:40:52 +0300

    Add add->sub instruction substitution for obfuscation pass

--- mypass.cpp
+++ mypass.cpp
@@ -3,3 +3,30 @@
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/IRBuilder.h"
+
+using namespace llvm;
+
+namespace {
+    //
+    struct InstructionSubtitution : public PassInfoMixin<InstructionSubtitution> {
+       PreservedAnalyses run(Function &F, FunctionAnalysisManager &) {
+           for (BasicBlock &BB: F) {
+               for (auto It = BB.begin(); It !=BB.end(); ++It) {
+                   Instruction &I = *It++;
+                    if (auto *Op = dyn_cast<BinaryOperator>(&I)) {
+                        if (Op->getOpcode() == Instruction::Add) {
+                            IRBuilder<> Builder(Op);
+                            Value* LHS = Op->getOperand(0);
+                            Value* RHS = Op->getOperand(1);
+                            Value* NegRHS = Builder.CreateNeg(RHS);
+                            Value* Sub = Builder.CreateSub(LHS, NegRHS);
+                            Op->replaceAllUsesWith(Sub);
+                            Op->eraseFromParent();
+                        }
+                    }
+               }
+           }
+       }
+    };
+}
